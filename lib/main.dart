a0f69922f8c336dceeafb74a9ec4a3eac73ba8f9
Dmitry Ilyin <mowgle>
2023-08-29 21:33:47 +0300

    feat: add controller to TextField

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _commentController = TextEditingController();
+
+  void _sendImage() {
+    final enteredComment = _commentController.text;
+    print(enteredComment);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _commentController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,13 +52,14 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: const [
-            ImageInput(),
-            SizedBox(height: 16),
+          children: [
+            const ImageInput(),
+            const SizedBox(height: 16),
             TextField(
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Comment',
               ),
+              controller: _commentController,
             )
           ],
         ),
@@ -53,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Send Image'),
         icon: const Icon(Icons.image_outlined),
-        onPressed: () {},
+        onPressed: _sendImage,
         tooltip: 'Increment',
       ),
     );
